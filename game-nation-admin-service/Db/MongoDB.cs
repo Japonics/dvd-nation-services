f8cf3f51ef8b3137afb10570b27f11ea17c55a66
Yellon1337 <yellon99@wp.pl>
2019-06-20 23:06:37 +0200

    Add models and DB

---
+++ game-nation-admin-service/Db/MongoDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using game_nation_admin_service.Models;
+
+namespace game_nation_admin_service.Db
+{
+    public class MongoDB
+    {
+        private readonly IMongoCollection<Categories> _categories;
+
+        public MongoDB()
+        {
+            var _client = new MongoClient("mongodb://localhost:27017");
+            var _db = _client.GetDatabase("GameNation");
+            _categories = _db.GetCollection<Categories>("Categories");
+        }
+
+        public Categories AddCategory(Categories cat)
+        {
+            _categories.InsertOne(cat);
+            return cat;
+        }
+
+        public void AttacheCategory(int id, Categories catIn)
+        {
+            _categories.ReplaceOne(cat => cat.Id == id, catIn);
+        }
+
+        public void DettachCategory(Categories catIn)
+        {
+            _categories.DeleteOne(cat => cat.Id == catIn.Id);
+        }
+
+        public void DettachCategory(int id)
+        {
+            _categories.DeleteOne(cat => cat.Id == id);
+        }
+    }
+}
---
+++ game-nation-admin-service/Models/Categories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace game_nation_admin_service.Models
+{
+    public class Categories
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _Id { get; set; }
+
+        [BsonElement("id")]
+        public int Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("description")]
+        public string Description { get; set; }
+        [BsonElement("color")]
+        public string Color { get; set; }
+        [BsonElement("icon")]
+        public string Icon { get; set; }
+    }
+}
